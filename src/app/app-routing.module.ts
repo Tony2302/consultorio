3fa0ecf240e07116e1ee223f071654c8ac5f40b1
Anthony-ui <morante2302@gmail.com>
2021-07-25 22:52:16 -0500

    subir

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { LoginComponent } from './Vistas/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './Vistas/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'login', component:LoginComponent},
+  {path:'menu', component:DashboardComponent},
+
+  {path:'',pathMatch:'full',redirectTo:'login'},
+  {path:'**',pathMatch:'full',redirectTo:'login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Vistas/login/login.component';
+import { DashboardComponent } from './Vistas/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
